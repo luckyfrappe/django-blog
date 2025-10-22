3388ceb0345789812236106f25195cd422f0d448
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-10-22 19:47:30 +0000

    Display success messages

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURIfalseTY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
@@ -48,12 +48,17 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     "django_summernote",
+    'crispy_forms',
+    'crispy_bootstrap5',
     "about",
 ]
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'https://www.google.com/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
