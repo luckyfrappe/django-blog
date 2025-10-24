72c27c2ce5364664262ca12973eb9d84893fd58b
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2025-10-24 06:42:12 +0000

    Create collaboration request

--- about/views.py
+++ about/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateRequestForm
+from django.contrib import messages
 
 def about_detail(request):
 
     about_content = About.objects.all().order_by("-updated_on").first()
 
+    if request.method == "POST":
+        print("Recieved a POST request")
+        collaboration_form = CollaborateRequestForm(data=request.POST)
+        if collaboration_form.is_valid():
+            collaboration_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Request submitted, we will get in touch shortly'
+            )
+    collaboration_form = CollaborateRequestForm()
+
     return render(
         request,
         "about/about_detail.html",
-        {"about_content": about_content},
+        {"about_content": about_content,
+         "collaboration_form": collaboration_form,},
     )
+
+ 
